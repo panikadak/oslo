9f6152ffb5f9e0603aaa83cbbaed77873de15419
jegraphy <jegraphy@gmail.com>
2023-08-07 13:25:50 +0300

    docs added

--- quasar.conf.js
+++ quasar.conf.js
@@ -50,6 +50,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
+      distDir: 'docs',
 
       // transpile: false,
 
